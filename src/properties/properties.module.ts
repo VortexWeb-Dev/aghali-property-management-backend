d15174d0d6f659269c0c6122ab7c3d56f4009902
fasilofficial <muhammedfasilofficial@gmail.com>
2025-01-23 11:33:56 +0530

    Bug fixes

--- src/properties/properties.module.ts
+++ src/properties/properties.module.ts
@@ -6,6 +6,7 @@ import { Property } from './entities/property.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Property])],
+  exports: [TypeOrmModule],
   controllers: [PropertiesController],
   providers: [PropertiesService],
 })
