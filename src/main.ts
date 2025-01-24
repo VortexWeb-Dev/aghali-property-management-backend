e84bbf918de8c428ba3256a9b1ec53ae100d0f04
fasilofficial <muhammedfasilofficial@gmail.com>
2025-01-24 10:15:07 +0530

    Enable preflight requests

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,29 @@ async function bootstrap() {
 
   const allowedOrigins = ['http://localhost', 'http://3.109.124.90'];
 
+  // Enable CORS
   app.enableCors({
     origin: allowedOrigins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: false,
+  });
+
+  // Enable CORS for preflight requests
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.status(204).send();
+    }
+    next();
   });
 
   const port = process.env.PORT || 3000;
