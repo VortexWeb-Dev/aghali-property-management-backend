d3db426984aba405904b0fceaf605eff8d14d69d
fasilofficial <muhammedfasilofficial@gmail.com>
2025-01-24 10:10:37 +0530

    Update CORS config

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigins = ['http://localhost', 'http://3.109.124.90'];
+
   app.enableCors({
-    origin: 'http://localhost',
+    origin: allowedOrigins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  })
-  await app.listen(process.env.PORT ?? 3000);
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: false,
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
